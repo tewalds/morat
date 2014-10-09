c4d8175947827f029272a8819c3368f4b514c980
Timo Ewalds <timo@ewalds.ca>
2014-10-09 00:47:29 -0400

    Use murmur3 instead of xorshift to mix bits. It does a much better job.

--- lib/bits.h
+++ lib/bits.h
@@ -7,17 +7,23 @@ namespace Morat {
 
 #define trailing_zeros(n) __builtin_ctz(n)
 
+// https://code.google.com/p/smhasher/wiki/MurmurHash3
 inline uint32_t mix_bits(uint32_t h){
-	h ^= (h << 13);
-	h ^= (h >> 17);
-	h ^= (h <<  5);
+	h ^= h >> 16;
+	h *= 0x85ebca6b;
+	h ^= h >> 13;
+	h *= 0xc2b2ae35;
+	h ^= h >> 16;
 	return h;
 }
 
+// https://code.google.com/p/smhasher/wiki/MurmurHash3
 inline uint64_t mix_bits(uint64_t h){
-	h ^= (h >> 17);
-	h ^= (h << 31);
-	h ^= (h >>  8);
+	h ^= h >> 33;
+	h *= 0xff51afd7ed558ccdull;
+	h ^= h >> 33;
+	h *= 0xc4ceb9fe1a85ec53ull;
+	h ^= h >> 33;
 	return h;
 }
 
